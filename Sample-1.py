c712735c7cfd071cd7cde2be5c73ec6655da7928
Ian <ianmchall@yahoo.com>
2022-09-14 19:43:58 -0400

    initial commit

---
+++ Sample-1.py
@@ -0,0 +1,17 @@
+print("Hello Wolrd!")
+
+list_trees = ["oak", "elm", "dogwood", "birch"]\
+
+print(list_trees [0]) #prints oak
+
+Tolkien_dict = {
+    "Novel" : "The Silmarilion",
+    "Year" : "1977",
+    "Author" : "J. R. R. Tolkien"
+}
+
+print(Tolkien_dict ["Author"])
+
+Mario_tuple = ("64", "Sunshine", "Galaxy", "Galaxy 2", "3D World", "Odyssey")
+
+print (Mario_tuple [0])
